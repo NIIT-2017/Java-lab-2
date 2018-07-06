c7b3e4d3f46b0c7f354509ee432904d5fcd866ae
PavelBelov <engoldo@gmail.com>
2018-07-06 20:13:53 +0300

    B1 fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+/**
+ * @author Pavel Belov
+ * http://www.shtanyuk.tk/edu/nniit/java-new/labs/labs2.html
+ * Lab №2
+ * Coffee vending emulator.
+ */
+
 public class Main {
     public static void main(String[] args) {
         Automata cafemonster = new Automata();
