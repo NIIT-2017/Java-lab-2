51719ff296dbebd24b5e0584800b93b503419164
agubin <tavridd@gmail.com>
2020-04-18 22:04:03 +0300

    the 4d commit

--- src/test/java/AutomataTest.java
+++ src/test/java/AutomataTest.java
@@ -24,95 +24,88 @@ public class AutomataTest {
     @Test
     public void work1() {
         baos.reset();
-        String testingstring = """
-            on
-            getmenu
-            coin 200
-            choice coffee
-            check
-            cook
-            exit""";
+        String testingstring = "on\n" +
+                               "getmenu\n" +
+                               "coin 200\n" +
+                               "choice coffee\n" +
+                               "check\n" +
+                               "cook\n" +
+                               "exit";
 
         bais = new ByteArrayInputStream(testingstring.getBytes());
         Atm = new Automata(bais, baos);
         Atm.work();
 
-        String answer = """
-                [Automate] The automate is disable. Possible actions : ON
-                [Client] on
-                [Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE
-                [Client] getmenu
-                [Automate] Menu:
-                [Automate] tea : 50
-                [Automate] coffee : 100
-                [Automate] juice : 150
-                [Client] coin 200
-                [Automate] You money is accepted. Possible actions : COIN, CHOICE, CANCEL, GETMENU, GETSTATE
-                [Automate] cash : 200.0
-                [Client] choice coffee
-                [Automate] You made a choice. Possible actions : CANCEL, CHECK, COOK, GETMENU, GETSTATE
-                [Client] check
-                [Automate] Cash is enough, you can cook the beverage
-                [Client] cook
-                [Automate] The cooking has started. Possible actions : just relax and wait
-                [Automate] cooking...
-                [Automate] Please, your coffee
-                [Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE
-                [Client] exit
-                """;
+        String answer = "[Automate] The automate is disable. Possible actions : ON\n" +
+                        "[Client] on\n" +
+                        "[Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE\n" +
+                        "[Client] getmenu\n" +
+                        "[Automate] Menu:\n" +
+                        "[Automate] tea : 50\n" +
+                        "[Automate] coffee : 100\n" +
+                        "[Automate] juice : 150\n" +
+                        "[Client] coin 200\n" +
+                        "[Automate] You money is accepted. Possible actions : COIN, CHOICE, CANCEL, GETMENU, GETSTATE\n" +
+                        "[Automate] cash : 200.0\n" +
+                        "[Client] choice coffee\n" +
+                        "[Automate] You made a choice. Possible actions : CANCEL, CHECK, COOK, GETMENU, GETSTATE\n" +
+                        "[Client] check\n" +
+                        "[Automate] Cash is enough, you can cook the beverage\n" +
+                        "[Client] cook\n" +
+                        "[Automate] The cooking has started. Possible actions : just relax and wait\n" +
+                        "[Automate] cooking...\n" +
+                        "[Automate] Please, your coffee\n" +
+                        "[Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE\n" +
+                        "[Client] exit\n";
         assertArrayEquals(answer.split("\r?\n"), baos.toString().split("\r?\n"));
     }
 
     @Test
     public void work2(){
         baos.reset();
-        String testingstring = """
-            on
-            coin 100
-            chiose juice
-            choice juice
-            cook
-            cancel
-            coin 100
-            choice juice
-            check
-            cook
-            """;
+        String testingstring = "on\n" +
+                               "coin 100\n" +
+                               "chiose juice\n" +
+                               "choice juice\n" +
+                               "cook\n" +
+                               "cancel\n" +
+                               "coin 100\n" +
+                               "choice juice\n" +
+                               "check\n" +
+                               "cook\n";
 
         bais = new ByteArrayInputStream(testingstring.getBytes());
         Atm = new Automata(bais, baos);
         Atm.work();
-        String answer = """
-                [Automate] The automate is disable. Possible actions : ON
-                [Client] on
-                [Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE
-                [Client] coin 100
-                [Automate] You money is accepted. Possible actions : COIN, CHOICE, CANCEL, GETMENU, GETSTATE
-                [Automate] cash : 100.0
-                [Client] chiose juice
-                [Automate] Unknown command "chiose"
-                [Client] choice juice
-                [Automate] You made a choice. Possible actions : CANCEL, CHECK, COOK, GETMENU, GETSTATE
-                [Client] cook
-                [Automate] Cash = 100.0
-                [Automate] Price of juice = 150
-                [Automate] You have to add 50.0
-                [Client] cancel
-                [Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE
-                [Client] coin 100
-                [Automate] You money is accepted. Possible actions : COIN, CHOICE, CANCEL, GETMENU, GETSTATE
-                [Automate] cash : 200.0
-                [Client] choice juice
-                [Automate] You made a choice. Possible actions : CANCEL, CHECK, COOK, GETMENU, GETSTATE
-                [Client] check
-                [Automate] Cash is enough, you can cook the beverage
-                [Client] cook
-                [Automate] The cooking has started. Possible actions : just relax and wait
-                [Automate] cooking...
-                [Automate] Please, your juice
-                [Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE
-                [Warning!] All input commands have red. Last command must be "exit". Forced termination.
-                """;
+        String answer = "[Automate] The automate is disable. Possible actions : ON\n" +
+                        "[Client] on\n" +
+                        "[Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE\n" +
+                        "[Client] coin 100\n" +
+                        "[Automate] You money is accepted. Possible actions : COIN, CHOICE, CANCEL, GETMENU, GETSTATE\n" +
+                        "[Automate] cash : 100.0\n" +
+                        "[Client] chiose juice\n" +
+                        "[Automate] Unknown command \"chiose\"\n" +
+                        "[Client] choice juice\n" +
+                        "[Automate] You made a choice. Possible actions : CANCEL, CHECK, COOK, GETMENU, GETSTATE\n" +
+                        "[Client] cook\n" +
+                        "[Automate] Cash = 100.0\n" +
+                        "[Automate] Price of juice = 150\n" +
+                        "[Automate] You have to add 50.0\n" +
+                        "[Client] cancel\n" +
+                        "[Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE\n" +
+                        "[Client] coin 100\n" +
+                        "[Automate] You money is accepted. Possible actions : COIN, CHOICE, CANCEL, GETMENU, GETSTATE\n" +
+                        "[Automate] cash : 200.0\n" +
+                        "[Client] choice juice\n" +
+                        "[Automate] You made a choice. Possible actions : CANCEL, CHECK, COOK, GETMENU, GETSTATE\n" +
+                        "[Client] check\n" +
+                        "[Automate] Cash is enough, you can cook the beverage\n" +
+                        "[Client] cook\n" +
+                        "[Automate] The cooking has started. Possible actions : just relax and wait\n" +
+                        "[Automate] cooking...\n" +
+                        "[Automate] Please, your juice\n" +
+                        "[Automate] The automate is waiting for your command. Possible action : OFF, COIN, GETMENU, GETSTATE\n" +
+                        "[Warning!] All input commands have red. Last command must be \"exit\". Forced termination.\n";
         assertArrayEquals(answer.split("\r?\n"), baos.toString().split("\r?\n"));
     }
 }
