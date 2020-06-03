7f835206322532006a844e950304d44b17297d15
tatyanashashalina <shash.tan@mail.ru>
2020-06-03 12:09:06 +0300

    Updated

--- src/test/java/AutomataTest.java
+++ src/test/java/AutomataTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AutomataTest {
 
-    @Test
+    @org.junit.Test
     void choice() {
         Automata TestAutomata = new Automata();
         TestAutomata.on();
